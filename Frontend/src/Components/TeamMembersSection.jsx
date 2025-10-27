c34b5ec4c27714ab75802aad123df4c2be6d6e2c
PIYUSH <218452820+mayo-byte07@users.noreply.github.com>
2025-10-27 22:51:05 +0530

    Update TeamMembersSection.jsx

--- Frontend/src/Components/TeamMembersSection.jsx
+++ Frontend/src/Components/TeamMembersSection.jsx
@@ -247,7 +247,7 @@ const TeamMembersSection = () => {
       },
       {
         id: 16,
-        name: "Piyush Kumar",
+        name: "Piyush Singh",
         position: "Core Team Member",
         bio: "UI/UX designer focused on creating intuitive and beautiful interfaces.",
         image: "https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
